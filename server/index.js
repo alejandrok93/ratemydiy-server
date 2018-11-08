b72e93f8b7e937deb36c800dde675a4d7968a1b4
alejandrok93 <alejandrok93@gmail.com>
2018-11-08 09:54:31 -0800

    added seeds file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8000;
 server.get("/", (req, res) => {
   res.send("server is running!");
 });
+
 server.listen(port, () => {
   console.log("server is running!");
 });
